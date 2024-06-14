d8cb53bfdb9015c0bc2ac4f10f75b1a8777026bf
Raymond Bian <raybbian@gmail.com>
2024-06-13 19:17:13 -0700

    Initial Commit

---
+++ adjv/adjvApp.swift
@@ -0,0 +1,17 @@
+//
+//  adjvApp.swift
+//  adjv
+//
+//  Created by Raymond Bian on 6/13/24.
+//
+
+import SwiftUI
+
+@main
+struct adjvApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
